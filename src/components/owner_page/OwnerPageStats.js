12df2af4b51330aa0445b86943ed056b98d80d74
wintee <b12e34e56@naver.com>
2023-05-22 03:49:52 +0900

    bar chart 테스트

--- src/components/owner_page/OwnerPageStats.js
+++ src/components/owner_page/OwnerPageStats.js
@@ -1,3 +1,6 @@
+import {OwnerPageSalesChart} from "./OwnerPageSalesChart";
+import {OwnerPageAmountChart} from "./OwnerPageAmountChart";
+
 function OwnerPageStats() {
     return (
         <div className="my-page-stats">
@@ -5,10 +8,12 @@ function OwnerPageStats() {
                 <div className="top-text">
                     메뉴별 판매수량
                 </div>
+                <OwnerPageAmountChart></OwnerPageAmountChart>
 
                 <div className="top-text">
                     요일별 총 매출
                 </div>
+                <OwnerPageSalesChart></OwnerPageSalesChart>
             </div>
         </div>
     )
