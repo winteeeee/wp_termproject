ebfc542165528e1bc9f694008d29bf65d9618cb4
chajungto <50168549+chajungto@users.noreply.github.com>
2023-06-02 09:24:48 +0900

    div 명칭 수정
    menutab-page-whole-div -> menutab-page-div
    menutab-page-title-div -> menutab-page-title

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import InfoModify from './components/info_modify/InfoModify';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <InfoModify />
+    <Review />
   </React.StrictMode>
 );
 
