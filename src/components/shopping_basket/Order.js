04fe02d1b33000d4efce7724c1b5d7350cb29850
Dori <qarkchanjin@gmail.com>
2023-06-05 01:42:49 +0900

    setState 문제

--- src/components/shopping_basket/Order.js
+++ src/components/shopping_basket/Order.js
@@ -11,29 +11,7 @@ function Order() {
     const plusBtnImg = "data:image/svg+xml;base64,PHN2ZyBpZD0iXyIgZGF0YS1uYW1lPSIrIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2MiIgaGVpZ2h0PSI2MiIgdmlld0JveD0iMCAwIDYyIDYyIj4KICA8ZGVmcz4KICAgIDxzdHlsZT4KICAgICAgLmNscy0xIHsKICAgICAgICBmaWxsOiAjZmZmOwogICAgICAgIHN0cm9rZTogIzQxYjZlNjsKICAgICAgICBzdHJva2Utd2lkdGg6IDJweDsKICAgICAgfQoKICAgICAgLmNscy0yLCAuY2xzLTMgewogICAgICAgIGZpbGw6ICM0MWI2ZTY7CiAgICAgIH0KCiAgICAgIC5jbHMtMiB7CiAgICAgICAgZmlsbC1ydWxlOiBldmVub2RkOwogICAgICB9CiAgICA8L3N0eWxlPgogIDwvZGVmcz4KICA8Y2lyY2xlIGNsYXNzPSJjbHMtMSIgY3g9IjMxIiBjeT0iMzEiIHI9IjMwIi8+CiAgPHBhdGggY2xhc3M9ImNscy0yIiBkPSJNNjI4LDg2N2gyNXYzSDYyOHYtM1ptMTEtMTFoM3YyNWgtM1Y4NTZaIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtNjA5IC04MzcpIi8+CiAgPHJlY3QgaWQ9IuyCrOqwge2YlV8yIiBkYXRhLW5hbWU9IuyCrOqwge2YlSAyIiBjbGFzcz0iY2xzLTMiIHg9IjMwIiB5PSIzMCIgd2lkdGg9IjMiIGhlaWdodD0iMyIvPgo8L3N2Zz4K"
     const deleteImg = "data:image/svg+xml;base64,PHN2ZyBpZD0i64W47Jej7KeAIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MiIgaGVpZ2h0PSI5MiIgdmlld0JveD0iMCAwIDkyIDkyIj4KICA8ZGVmcz4KICAgIDxzdHlsZT4KICAgICAgLmNscy0xIHsKICAgICAgICBmaWxsOiBub25lOwogICAgICB9CgogICAgICAuY2xzLTIsIC5jbHMtMyB7CiAgICAgICAgZmlsbDogIzAwMDAwMDsKICAgICAgfQoKICAgICAgLmNscy0yIHsKICAgICAgICBmaWxsLXJ1bGU6IGV2ZW5vZGQ7CiAgICAgIH0KICAgIDwvc3R5bGU+CiAgPC9kZWZzPgogIDxjaXJjbGUgY2xhc3M9ImNscy0xIiBjeD0iNDYiIGN5PSI0NiIgcj0iNDYiLz4KICA8cGF0aCBpZD0iYnRuX3giIGNsYXNzPSJjbHMtMiIgZD0iTTI3My4wMTQsNzAxTDI3NSw2OTkuMDEzLDMwMi45ODYsNzI3LDMwMSw3MjguOTg2Wm0yOS45NzIsMEwzMDEsNjk5LjAxMywyNzMuMDE0LDcyNywyNzUsNzI4Ljk4NloiIHRyYW5zZm9ybT0idHJhbnNsYXRlKC0yNDIgLTY2OCkiLz4KICA8cmVjdCBpZD0i7IKs6rCB7ZiVXzEiIGRhdGEtbmFtZT0i7IKs6rCB7ZiVIDEiIGNsYXNzPSJjbHMtMyIgeD0iNDQiIHk9IjQ0IiB3aWR0aD0iNCIgaGVpZ2h0PSI0Ii8+Cjwvc3ZnPgo="
     let [pizzaInfo, setPizzaInfo] = useState([])
-    const [numberCount, setNumberCount] = useState(1)
-    const countArray = []
-    let price = 0;
-    const [totalPrice, setTotalPrice] = useState(0)
-
-
-    const clickMinusBtn = () => {
-        if (numberCount > 1 ) {
-            setNumberCount(numberCount - 1)
-
-        }
-    }
-
-    const clickPlusBtn = () => {
-        if (numberCount < 10) {
-            setNumberCount(numberCount + 1)
-
-        }
-    }
-
-    const priceCheck = (price) => {
-        setTotalPrice(totalPrice + price)
-    }
+    const [price, setPrice] = useState(0)
 
     useEffect(() => {
         fetchData().then();
@@ -52,15 +30,15 @@ function Order() {
 return (
     <body>
     {
-        pizzaInfo.map((pizzaInfo, index) => (
-            <MyMenuList key={index} pizzaInfo={pizzaInfo} numberCount = {numberCount} clickMinusBtn={clickMinusBtn} clickPlusBtn={clickPlusBtn} priceCheck = {priceCheck}></MyMenuList>
+        pizzaInfo.map((pizzaInfo) => (
+            <MyMenuList pizzaInfo={pizzaInfo} setPrice = {setPrice} price = {price}></MyMenuList>
         ))
     }
     <div className="total-layout">
         <h4 className="sum-total">합계</h4>
         <h4 className="total">
             총
-            <font className="total-price">{totalPrice}원</font>
+            <font className="total-price">{price}원</font>
             원
         </h4>
         <div className="basic-button">주문하기</div>
